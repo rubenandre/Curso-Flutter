caa4f5e2b51b9c7835f715e0303681d1542d5380
Rúben Silva <rubensilva945@gmail.com>
2019-12-25 21:59:02 +0000

    Create database structure

--- agenda_contactos/lib/helpers/contact_helper.dart
+++ agenda_contactos/lib/helpers/contact_helper.dart
@@ -1,5 +1,40 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:async/async.dart';
+import 'package:agenda_contactos/model/Contact.dart';
+import 'contact_columns.dart';
 
 class ContactHelper {
+
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDB();
+    return _db;
+  }
+
+  Future<Database> initDB() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+        "CREATE TABLE contactTable("
+            "$ContactColumns.idColumn INTEGER PRIMARY KEY, "
+            "$ContactColumns.nameColumn TEXT, "
+            "$ContactColumns.emailColumn TEXT, "
+            "$ContactColumns.phoneColumn TEXT, "
+            "$ContactColumns.imgColumn TEXT)"
+      );
+    });
+  }
 
 }
