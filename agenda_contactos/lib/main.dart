cbca4807c7b16cfa136254d8075f904a60bc1bdb
Rúben Silva <rubensilva945@gmail.com>
2019-12-25 23:38:42 +0000

    Inited ContactsPage design.

--- agenda_contactos/lib/main.dart
+++ agenda_contactos/lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:agenda_contactos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:agenda_contactos/UI/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ContactPage(),
   ));
 }
